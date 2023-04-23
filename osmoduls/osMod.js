e3acbc993414bdbb7fe798448e671f1585d1d514
mirajpatel752 <mirajpatel752@gmail.com>
2023-04-23 17:41:33 +0530

    crod

---
+++ osmoduls/osMod.js
@@ -0,0 +1,21 @@
+const os = require("os")
+
+console.log(os.arch())
+
+console.log(os.hostname())
+console.log(os.platform())
+console.log(os.tmpdir)
+console.log(os.type())
+
+
+
+
+
+
+// const free = os.freemem()
+// console.log(free)
+// console.log(`${free /1024 / 1024 /1024 }`)
+
+
+// const freeTotal = os.totalmem()
+// console.log(`${freeTotal /1024 / 1024 /1024 }`)
