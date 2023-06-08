e24e0472a04fd546dd95f7b1e262615f416f503a
miraj  patel <96917977+mirajpatel752@users.noreply.github.com>
2023-06-08 21:09:18 +0530

    mongodb conect

---
+++ mogos/src/app.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+
+
+
+const dbURI = 'mongodb://localhost:27017/local';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
