02c4f6dd41aabb4c4cb9c0b9ae07f4b9628da414
mirajpatel752 <mirajpatel752@gmail.com>
2023-10-15 12:33:28 +0530

    node .js mongodb

--- mogos/src/app.js
+++ mogos/src/app.js
@@ -1,14 +1,26 @@
 const mongoose = require("mongoose");
 
+// mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopoLogy: true, useFindAndModify: true })
+//   .then(() => console.log("complledt..."))
+//   .catch((err) => console.log(err));
 
 
+const URI = process.env.MONGODB_URL;
 
-const dbURI = 'mongodb://localhost:27017/local';
+// mongoose.connect(URI, {
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-  });
+// useNewUrlParser: true, 
+
+// useUnifiedTopology: true 
+
+// }, err => {
+// if(err) throw err;
+// console.log('Connected to MongoDB!!!')
+// });
+
+// const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URL;
+const dbName = 'miraj';
+
+mongoose.connect("mongodb://localhost:27017/miraj", { useNewUrlParser: true })
