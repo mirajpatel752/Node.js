5d1d48d6e38e6cbf17f3d06e4478ece61d571e74
mirajpatel752 <mirajpatel752@gmail.com>
2024-03-02 00:30:05 +0530

    hbs website express

--- expresh.js/src/index.js
+++ expresh.js/src/index.js
@@ -23,39 +23,69 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+var requests = require("requests");
 const Port = 8000;
 
 const staticPath = path.join(__dirname, "../public");
-const templed = path.join(__dirname,"../templed")
+const templed = path.join(__dirname, "../templed");
 
 // app.use(express.static(staticPath))
 
 app.set("views engine", "hbs");
 
-app.set('views',templed)
+app.set("views", templed);
 
 // app.get("/", (req, res) => {
 //   res.render("index");
 // });
 
-
-
 app.get("/", (req, res) => {
   res.send("hello  friend");
 });
 
-
 app.get("/about", (req, res) => {
+  requests(
+    `https://api.openweathermap.org/data/2.5/weather?q=${"surat"}&appid=${"a25f91de011ddf324975b16ebe57c8a2"}`
+  ).on("data", (chunk) => {
+    const objData = JSON.parse(chunk);
+    const arrdata = [objData];
+    console.log(`city ${arrdata[0].name} and  ${arrdata[0].main.tems}`);
+    // const realTimeData =arrdata
+    // .map((val)=> replaceVal(homeFile,val)
+    // .join("")
+    // )
+    // res.write(realTimeData);
+  });
+  on("end", (err) => {
+    if (err) return console.log(err);
+    res.end();
+  });
   res.send("hello about  friend");
 });
 
 
-app.get("*",(req,res)=>{
-  res.send("404")
-})
+app.get("/home", (req, res) => {
+  requests(
+    `https://api.openweathermap.org/data/2.5/weather?q=${"surat"}&appid=${"a25f91de011ddf324975b16ebe57c8a2"}`
+  )
+  .on('data', function (chunk) {
+    const objData = JSON.parse(chunk);
+    const arrdata = [objData];
+    console.log(`city ${arrdata[0].name} and  ${arrdata[0].main.temp}`);
+  })
+  .on('end', function (err) {
+    if (err) return console.log('connection closed due to errors', err);
+    console.log('end');
+  });
+  res.send("hello about  friend");
+});
+
+
+
+app.get("*", (req, res) => {
+  res.send("404");
+});
 
 app.listen(Port, () => {
   console.log("listening to the post");
 });
-
-
