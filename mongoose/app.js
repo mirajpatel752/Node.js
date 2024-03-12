3556e643d95fb88088c73285255dca4ce528f76f
mirajpatel752 <mirajpatel752@gmail.com>
2024-03-12 21:13:06 +0530

    curd mongo db

--- mongoose/app.js
+++ mongoose/app.js
@@ -1,78 +1,119 @@
-const connectDB = require('./mongoose');
-const express = require('express');
-const mongoose = require('mongoose'); 
+const connectDB = require("./mongoose");
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 connectDB();
 
 const playlistSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    ctype: String,
-    videos: Number,
-    author: String,
-    active: Boolean,
-    date: {
-        type: Date,
-        default: Date.now
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  ctype: String,
+  videos: Number,
+  author: String,
+  active: Boolean,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const Playlist = mongoose.model("Playlist", playlistSchema);
 
-const createDocument   = async ()=>{
-   try {
+const createDocument = async () => {
+  try {
     const reactPlayList = new Playlist({
-      name:"React",
-      video:23,
+      name: "React",
+      video: 23,
       author: "a",
-      active:true,
-    }) 
+      active: true,
+    });
     const javaPlayList = new Playlist({
-      name:"java",
-      video:23,
+      name: "java",
+      video: 23,
       author: "b",
-      active:false,
-    }) 
+      active: false,
+    });
     const nodePlayList = new Playlist({
-      name:"node",
-      video:23,
+      name: "node",
+      video: 23,
       author: "c",
-      active:true,
-    }) 
+      active: true,
+    });
     const mongoPlayList = new Playlist({
-      name:"mongo",
-      video:20,
+      name: "mongo",
+      video: 20,
       author: "d",
-      active:true,
-    }) 
+      active: true,
+    });
     // const result =  await reactPlayList.save();
-    const result = await Playlist.insertMany([reactPlayList,javaPlayList,nodePlayList,mongoPlayList])
-    console.log(result,"@")
-    
-   } catch (error) {
-    console.log(error,"ERR")
-    
-   }
-}
+    const result = await Playlist.insertMany([
+      reactPlayList,
+      javaPlayList,
+      nodePlayList,
+      mongoPlayList,
+    ]);
+    console.log(result, "@");
+  } catch (error) {
+    console.log(error, "ERR");
+  }
+};
 // createDocument()
 
-
-const getDocument = async ()=>{
+const getDocument = async () => {
   // const result = await Playlist.find()
   // const result = await Playlist.find({active:false})
   // const result = await Playlist.find({video:{$gte:50}})
   // const result = await Playlist.find({$or:[{author:"miraj"},{active:false}]})
-  const result = await Playlist.find({$and:[{author:"miraj"},{active:true}]})
-
-
+  // const result = await Playlist.find({$and:[{author:"miraj"},{active:true}]})
+  const result = await Playlist.find({ author: "miraj" })
+    // .select({name:1})
+    .sort({ name: -1 });
   // .select({name:1})
   // .limit(1)
-  console.log(result,"$@#")
-}
-getDocument()
+  console.log(result, "$@#");
+};
+// getDocument()
 
+const updateDocument = async (_id) => {
+  try {
+    // const  result = await Playlist.updateOne({_id},{
+    //   $set : {
+    //     name:'BHNADERI'
+    //   }
+    // })
+    const result = await Playlist.findByIdAndUpdate(
+      { _id },
+      {
+        $set: {
+          name: "bhanderi",
+        },
+      },
+      {
+        new: true,
+        useFindAndModify: false,
+      }
+    );
+    console.log(result, "#");
+  } catch (error) {}
+};
+
+// updateDocument("65eb5cf75c055f203acc24ce")
+
+const deleteDocument = async (_id) => {
+  try {
+      const Result = await Playlist.deleteOne({_id})
+    
+  } catch (error) {
+    console.log(error,"error")
+    
+  }
+};
+
+deleteDocument("65eb5a883d66e684d164d9d1");
+
+//delete the document
 
 // Define a route handler to save the playlist
 // app.post('/post', async (req, res) => {
@@ -91,6 +132,3 @@ getDocument()
 // app.listen(PORT, () => {
 //     console.log(`Server is running on port ${PORT}`);
 // });
-
-
-
